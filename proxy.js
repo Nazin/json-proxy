83f7daab3497a58950af37c700eb536bbaf5a1bd
Piotr Niziniecki <Piotr.Niziniecki@sabre.com>
2017-08-26 16:26:00 +0100

    Added possibility to not send the RQ at all.

--- proxy.js
+++ proxy.js
@@ -9,7 +9,8 @@ export function createProxyServer() {
     console.log(`Serving: ${req.method} ${req.url}`);
 
     const configuration = JSON.parse(fs.readFileSync('./config.json', 'utf8'));
-    const rules = (configuration.rules || []).filter(rule => rule.enabled && req.method === rule.method && req.url.match(rule.url));
+    const matchingURLs = (configuration.urls || []).filter(url => url.enabled && req.method === url.method && req.url.match(url.url));
+    const urlConfiguration = matchingURLs.length !== 0 ? matchingURLs[0] : undefined;
     const proxy = configuration.proxy.enabled ? configuration.proxy.url : undefined;
 
     let requestBody = '';
@@ -19,13 +20,11 @@ export function createProxyServer() {
     });
 
     req.on('end', () => {
-      const adjustingRQRules = rules.filter(rule => rule.adjust === 'RQ');
-
       let newRequestBody;
 
       try {
         newRequestBody = requestBody ? JSON.parse(requestBody) : requestBody;
-        const tmp = adjustBody(newRequestBody, undefined, adjustingRQRules);
+        const tmp = adjustBody(newRequestBody, undefined, urlConfiguration && urlConfiguration.rules || []);
         newRequestBody = tmp[0];
         newRequestBody = newRequestBody ? JSON.stringify(newRequestBody) : newRequestBody;
       } catch (e) {
@@ -37,6 +36,11 @@ export function createProxyServer() {
       headers.host = configuration.endpoint.replace('https://', '');
       delete headers['accept-encoding'];
 
+      if (urlConfiguration && !urlConfiguration.sendRQ) {
+        adjustAndSendResponse(res, {statusCode: 200}, requestBody, urlConfiguration);
+        return;
+      }
+
       const options = {
         url: configuration.endpoint + req.url,
         method: req.method,
@@ -46,35 +50,10 @@ export function createProxyServer() {
       };
 
       request(options, (error, response) => {
-        const adjustingRSRules = rules.filter(rule => rule.adjust === 'RS');
-
         if (error) {
           console.log(error);
         }
-
-        let responseBody;
-        let responseStatusCode = response && response.statusCode || 500;
-
-        try {
-          responseBody = response.body ? JSON.parse(response.body) : response.body;
-          const tmp = adjustBody(requestBody ? JSON.parse(requestBody) : requestBody, responseBody, adjustingRSRules, responseStatusCode);
-          responseBody = tmp[1];
-          responseStatusCode = tmp[2];
-          responseBody = responseBody ? JSON.stringify(responseBody) : responseBody;
-        } catch (e) {
-          console.log('Problem with adjusting the response');
-          responseBody = response && response.body || JSON.stringify({status: 'ERROR'});
-        }
-
-        const responseHeaders = JSON.parse(JSON.stringify(response && response.headers || {"Content-Type": "application/json"}));
-        if (responseHeaders['set-cookie']) {
-          responseHeaders['set-cookie'] = responseHeaders['set-cookie'].map(cookie => cookie.replace('Secure; HttpOnly', ''));
-        }
-        delete responseHeaders['content-length'];
-
-        res.writeHead(responseStatusCode, responseHeaders);
-        res.write(responseBody);
-        res.end();
+        adjustAndSendResponse(res, response, requestBody, urlConfiguration);
       });
     });
   });
@@ -95,24 +74,49 @@ export function stopProxyServer() {
 }
 
 function adjustBody(requestBody, responseBody, rules, responseStatusCode) {
-  rules.forEach((rule) => {
-    rule.rules.forEach((detailedRule) => {
-      try {
-        if (eval(detailedRule.condition)) {
-          console.log(`Condition "${detailedRule.condition}" valid`);
-          detailedRule.actions.forEach((action) => {
-            console.log(`Applying rule: ${action}`);
-            try {
-              eval(action);
-            } catch (e) {
-              console.log('invalid rule');
-            }
-          });
-        }
-      } catch (e) {
-        console.log(`invalid condition: "${detailedRule.condition}"`);
+  rules.forEach((detailedRule) => {
+    try {
+      if (eval(detailedRule.condition)) {
+        console.log(`Condition "${detailedRule.condition}" valid`);
+        detailedRule.actions.forEach((action) => {
+          console.log(`Applying rule: ${action}`);
+          try {
+            eval(action);
+          } catch (e) {
+            console.log('invalid rule');
+          }
+        });
       }
-    });
+    } catch (e) {
+      console.log(`invalid condition: "${detailedRule.condition}"`);
+    }
   });
   return [requestBody, responseBody, responseStatusCode];
+}
+
+function adjustAndSendResponse(res, response, requestBody, urlConfiguration) {
+
+  let responseBody;
+  let responseStatusCode = response && response.statusCode || 500;
+
+  try {
+    responseBody = response.body ? JSON.parse(response.body) : response.body;
+    const tmp = adjustBody(requestBody ? JSON.parse(requestBody) : requestBody, responseBody, urlConfiguration && urlConfiguration.rules || [], responseStatusCode);
+    responseBody = tmp[1];
+    responseStatusCode = tmp[2];
+    responseBody = responseBody ? JSON.stringify(responseBody) : responseBody;
+  } catch (e) {
+    console.log('Problem with adjusting the response');
+    responseBody = response && response.body || JSON.stringify({status: 'ERROR'});
+  }
+
+  const responseHeaders = JSON.parse(JSON.stringify(response && response.headers || {"Content-Type": "application/json"}));
+  if (responseHeaders['set-cookie']) {
+    responseHeaders['set-cookie'] = responseHeaders['set-cookie'].map(cookie => cookie.replace('Secure; HttpOnly', ''));
+  }
+  delete responseHeaders['content-length'];
+
+  res.writeHead(responseStatusCode, responseHeaders);
+  res.write(responseBody);
+  res.end();
 }
