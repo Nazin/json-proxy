82ceea8f6bbe771abf517662e7ab30434909c49b
Piotr Niziniecki <Piotr.Niziniecki@sabre.com>
2017-12-22 12:25:00 +0100

    Adding proxy and compression configuration for express

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import compression from 'compression';
 import cors from 'cors';
 import bodyParser from 'body-parser';
 import controllers from './controllers';
@@ -6,6 +7,9 @@ import controllers from './controllers';
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 
 const app = express();
+
+app.set('trust proxy', true);
+app.use(compression());
 
 app.use(bodyParser.json({ limit: '50mb' }));
 app.use(bodyParser.urlencoded({ extended: true }));
