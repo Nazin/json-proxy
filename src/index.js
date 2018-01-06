e6a67ca3ed99b78ece6268eac52481b8f3a03867
Piotr Niziniecki <Piotr.Niziniecki@sabre.com>
2018-01-06 09:24:00 +0100

    Added config.json file to configure basic proxy behavior like port, body limit and cors

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import http from 'http';
 import Server from './server';
+import config from '../config.json';
 
 const httpServer = http.createServer(Server);
 
-httpServer.listen(3005, (error) => {
+httpServer.listen(config.port, (error) => {
   if (error) {
     return console.error(error);
   }
-  return console.log('Proxy server started, listening at http://localhost:3005. Configuration UI available at http://localhost:3005/config.');
+  return console.log(`Proxy server started, listening at http://localhost:${config.port}. Configuration UI available at http://localhost:${config.port}/config.`);
 });
--- src/server.js
+++ src/server.js
@@ -4,21 +4,23 @@ import compression from 'compression';
 import cors from 'cors';
 import bodyParser from 'body-parser';
 import controllers from './controllers';
+import config from '../config.json';
 
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 process.env.ROOT = path.join(__dirname, '..');
 
 const app = express();
 
+const corsConfig = Object.assign({}, config.cors, {
+  origin: (ignored, callback) => callback(null, true),
+});
+
 app.set('trust proxy', true);
 app.use(compression());
 
-app.use(bodyParser.json({ limit: '50mb' }));
+app.use(bodyParser.json({ limit: config.bodyParserLimit }));
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cors({
-  origin: (ignored, callback) => callback(null, true),
-  credentials: true,
-}));
+app.use(cors(corsConfig));
 
 app.use(controllers());
 
