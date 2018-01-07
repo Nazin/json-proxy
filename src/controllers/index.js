b264c26ed07c54fb7fe20e8855c11aeab1b34fb6
Piotr Niziniecki <Piotr.Niziniecki@sabre.com>
2018-01-07 16:12:00 +0100

    Added favicon

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,14 +1,16 @@
-import { Router } from 'express';
+import express from 'express';
 import configController from './config';
 import proxyController from './proxy';
 import config from '../../config.json';
 
 export default () => {
-  const router = new Router();
+  const router = new express.Router();
 
   if (config.configUI.enabled) {
     router.use(config.configUI.endpoint, configController());
   }
+
+  router.use('/favicon.ico', express.static('./public/images/favicon.ico'));
   router.use('/', proxyController());
 
   return router;
