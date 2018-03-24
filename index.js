224e4f040dba0411fb6c82b13b5a97b62fb3fae1
Piotr Niziniecki <Piotr.Niziniecki@sabre.com>
2018-03-24 15:24:00 +0100

    Moving files around

---
+++ index.js
@@ -0,0 +1,13 @@
+import http from 'http';
+import Server from './src/server';
+import serverConfig from './server-config.json';
+
+const httpServer = http.createServer(Server);
+
+httpServer.listen(serverConfig.port, (error) => {
+  if (error) {
+    return console.error(error);
+  }
+  const configurationInfo = serverConfig.configUI.enabled ? `Configuration UI available at http://localhost:${serverConfig.port}${serverConfig.configUI.endpoint}.` : '';
+  return console.log(`Proxy server started, listening at http://localhost:${serverConfig.port}. ${configurationInfo}`);
+});
