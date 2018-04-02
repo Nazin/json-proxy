88e693eb4b99ade809f92a3f83d6684c0f8cdc46
Piotr Niziniecki <Piotr.Niziniecki@sabre.com>
2018-04-02 21:31:00 +0100

    Possibility to run the server on https

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
+import path from 'path';
+import spdy from 'spdy';
 import http from 'http';
+import fs from 'fs';
 import Server from './server/server';
 import serverConfig from './server-config.json';
 
-const httpServer = http.createServer(Server);
+const options = {
+  key: fs.readFileSync(path.join('.', 'certificates', 'server.key')),
+  cert: fs.readFileSync(path.join('.', 'certificates', 'server.crt')),
+};
 
-httpServer.listen(serverConfig.port, (error) => {
+const httpServer = serverConfig.https ? spdy.createServer(options, Server) : http.createServer(Server);
+
+httpServer.listen(process.env.PORT || serverConfig.port, (error) => {
   if (error) {
     return console.error(error);
   }
-  const configurationInfo = serverConfig.configUI.enabled ? `Configuration UI available at http://localhost:${serverConfig.port}${serverConfig.configUI.endpoint}.` : '';
-  return console.log(`Proxy server started, listening at http://localhost:${serverConfig.port}. ${configurationInfo}`);
+  const protocol = serverConfig.https ? 'https' : 'http';
+  const configurationInfo = serverConfig.configUI.enabled ? `Configuration UI available at ${protocol}://localhost:${process.env.PORT || serverConfig.port}${serverConfig.configUI.endpoint}.` : '';
+  return console.log(`Proxy server started, listening at ${protocol}://localhost:${process.env.PORT || serverConfig.port}. ${configurationInfo}`);
 });
