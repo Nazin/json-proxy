9b35c0e13cf76a6f33f3e9d0d5023e405b0b95fb
Piotr Niziniecki <Piotr.Niziniecki@sabre.com>
2018-05-20 10:49:41 +0100

    Improved file loading

--- server/proxy-controller/loadFile.js
+++ server/proxy-controller/loadFile.js
@@ -2,19 +2,13 @@ const fs = require('fs');
 const path = require('path');
 
 module.exports = function loadFromFile(filePath) {
-  console.log('File path: ', filePath);
   let content;
-  if (!filePath) {
-    content = { status: 'ERROR', message: 'Missing responseFilePath' };
-    return content;
-  }
   try {
-    const fullFilePath = path.join(process.env.ROOT, filePath);
-    content = fs.readFileSync(fullFilePath, { encoding: 'utf8' });
+    const fullFilePath = path.resolve(filePath);
+    content = fs.readFileSync(fs.existsSync(fullFilePath) ? fullFilePath : path.resolve(__dirname, filePath), { encoding: 'utf8' });
   } catch (e) {
     console.log('Having problem when reading data file', e);
     content = { status: 'ERROR', message: 'Having problem when reading data file' };
-    console.log('content type of content', typeof content);
     return content;
   }
   return JSON.parse(content);
